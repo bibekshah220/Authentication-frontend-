b2947c189ded61535dd07f8edeb6867ed9b14c21
bibekshah220 <bibekshah425@gmail.com>
2026-03-23 22:47:04 +0545

    feat: implement routing with BrowserRouter and add main application routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import VerifyOtp from './pages/verifyotp'
+import Verify from './pages/Verify'
+import Dashboard from './pages/Dashboard'
+
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/verify-otp" element={<VerifyOtp />} />
+        <Route path="/verify" element={<Verify />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
