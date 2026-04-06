753151212390b539851b1e1a638dc60c78ca1701
bibekshah220 <bibekshah425@gmail.com>
2026-04-06 23:16:12 +0545

    feat: initialize frontend authentication boilerplate with Tailwind CSS and routing pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import VerifyOtp from './pages/verifyotp'
-import Verify from './pages/Verify'
-import Dashboard from './pages/Dashboard'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 
+import Home from './pages/home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import VerifyOtp from './pages/verifyotp';
+import Dashboard from './pages/Dashboard';
+import VerifyEmail from './pages/VerifyEmail';
+import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/verify-otp" element={<VerifyOtp />} />
-        <Route path="/verify" element={<Verify />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/verify-otp" element={<VerifyOtp />} />
+          <Route path="/token/:token" element={<VerifyEmail />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
 
-export default App
+          {/* Protected Routes */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer position="bottom-right" theme="dark" />
+    </AuthProvider>
+  );
+};
+
+export default App;
